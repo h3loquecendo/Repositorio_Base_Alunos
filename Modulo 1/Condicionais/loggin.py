3391c1cf1025f5e8c9a3fc92dfbb7f815981c95e
h3loquecendo <heloisavieira.vieira28@gmail.com>
2025-09-12 14:16:25 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/loggin.py
@@ -0,0 +1,11 @@
+print("-----------------------")
+print("---sistema de loggin---")
+print("-----------------------")
+
+usuario = input("digite seu usuario: ")
+senha = input("digite sua senha: ")
+
+if usuario == "helo" and senha == "helo123":
+    print("logado")
+else:
+    print("usuario ou senha incorreta")
